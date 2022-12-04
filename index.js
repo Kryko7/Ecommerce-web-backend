0b922886606934baf6e6ee53690513c52fbf6f9e
Kryko7 <61829993+Kryko7@users.noreply.github.com>
2022-12-04 10:51:48 +0530

    Connect Database
    MySQL Database is connected to the project

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const express = require("express");
 
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: 'password',
+  database: 'project'
+});
+
+connection.connect(err => {
+    if (err) throw err;
+    console.log('Connected to database');
+});
+
 const app = express();
 app.get("/", (req, res) => {
     res.send("Hello Project");
