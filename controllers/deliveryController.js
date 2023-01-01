f1cd19c57cac1c8da717190ba7678a8b61a76c4e
Kryko7 <61829993+Kryko7@users.noreply.github.com>
2023-01-01 20:17:11 +0530

    Basic Implementation Complete

--- controllers/deliveryController.js
+++ controllers/deliveryController.js
@@ -6,22 +6,32 @@ const router = express.Router();
 const connection = require("../services/db");
 
 
-router.post('/delay', (req, res) => {
+router.post('/delay', async(req, res) => {
     const {mainCity, inStock} = req.body;
+    let orderStatus = "in progress";
     let deliveryDelay = 0;
-    if (mainCity === true && inStock === true){
+    if (mainCity === "true" && inStock === "true"){
         deliveryDelay = 5;
-    } else if (mainCity === true && inStock === false){
+        orderStatus = "Processing-1"
+    } else if (mainCity === "true" && inStock === "false"){
         deliveryDelay = 8;
+        orderStatus = "Processing-2"
     }
-    else if (mainCity === false && inStock === true){
+    else if (mainCity === "false" && inStock === "true"){
         deliveryDelay = 7;
+        orderStatus = "Processing-3"
     }
-    else if (mainCity === false && inStock === false){
+    else if (mainCity === "false" && inStock === "false"){
         deliveryDelay = 10;
+        orderStatus = "Processing-4"
     }
-    res.send({deliveryDelay});
+
+    sendResponse(deliveryDelay, orderStatus, res);
 });
+
+function sendResponse(deliveryDelay, orderStatus, res){
+    res.send({deliveryDelay, orderStatus});
+}
 
 
 router.post('/town', (req, res) => {
@@ -33,18 +43,55 @@ router.post('/town', (req, res) => {
 });
 
 
+// router.post('/stock-check', (req, res) => {
+//     cart = req.body;
+//     var inStock = true;
+//     for (let i = 0; i < cart.length; i++) {
+//         connection.query("SELECT quantity FROM product_item WHERE product_item_id = ?", [cart[i].product_item_id], (err, result) => {
+//             if (err) throw err;
+//             console.log(result[0].quantity);
+//             if (cart[i].quantity > result[0].quantity) {
+//                 console.log(cart[i].quantity);
+//                 console.log(result[0].quantity);
+            
+//                 inStock = false;
+//                 console.log(inStock);
+//             }
+//         });
+//     }
+//     console.log(inStock);
+//     res.send({ inStock });
+
+// })
+
 router.post('/stock-check', (req, res) => {
     cart = req.body;
     let inStock = true;
-    for (let i = 0; i < cart.length; i++) {
-        if (cart[i].quantity < `SELECT quantity FROM product WHERE product_id = ?`, cart[i].id) {
+  
+    function checkStock(callback) {
+      let completed = 0;
+      for (let i = 0; i < cart.length; i++) {
+        connection.query("SELECT quantity FROM product_item WHERE product_item_id = ?", [cart[i].product_item_id], (err, result) => {
+          if (err) throw err;
+          console.log(result[0].quantity);
+          if (cart[i].quantity > result[0].quantity) {
             inStock = false;
-            break;
-        }
+          }
+          completed++;
+          if (completed === cart.length) {
+            callback();
+          }
+        });
+      }
     }
-    res.send({ inStock });
-
-})
+  
+    checkStock(function() {
+      console.log(inStock);
+      res.send({ inStock });
+    });
+  });
+  
+  
 
 
 
